1ff26ecf4ca1de6990f38b4cda0d30888f9276ef
memorydream <34763046+memorydream@users.noreply.github.com>
2022-02-18 00:46:40 +0800

    fix: Keyboard input GC alloc

--- Cyan-Stars/Assets/Scripts/Input/InputProviderSO.cs
+++ Cyan-Stars/Assets/Scripts/Input/InputProviderSO.cs
@@ -13,14 +13,28 @@ namespace CyanStars.Input
         /// <returns>float - pos(0-1)  </returns>
         /// <returns>InputState - state  </returns>
         /// <returns>string - tag</returns>
-        public IEnumerable<(float, InputState, string)> Inputs => GetInputIter();
+        public IEnumerable<InputData> Inputs => GetInputIter();
         
         
-        protected abstract IEnumerable<(float, InputState, string)> GetInputIter();
+        protected abstract IEnumerable<InputData> GetInputIter();
     }
 
     public enum InputState
     {
         Down, Up
+    }
+
+    public struct InputData
+    {
+        public float Pos;
+        public InputState State;
+        public string Tag;
+
+        public InputData(float pos, InputState state, string tag)
+        {
+            this.Pos = pos;
+            this.State = state;
+            this.Tag = tag;
+        }
     }
 }
--- Cyan-Stars/Assets/Scripts/InputKeyControl.cs
+++ Cyan-Stars/Assets/Scripts/InputKeyControl.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using CyanStars.Input;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class InputKeyControl : MonoBehaviour
 {
@@ -18,21 +19,27 @@ public class InputKeyControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach (var (keyPos, state, keyTag) in m_inputProviderSO.Inputs)
+#if UNITY_EDITOR
+        Profiler.BeginSample("CyanStars.Input.Keyboard.Iter");
+#endif
+        foreach (var data in m_inputProviderSO.Inputs)
         {
-            if (state == InputState.Down && !_keyDict.ContainsKey(keyTag))
+            if (data.State == InputState.Down && !_keyDict.ContainsKey(data.Tag))
             {
                 var key = Instantiate(m_prefabKey);
-                key.name = keyTag;
-                key.transform.position = new Vector3(28 * keyPos - 14, 0.02f, -50f);
-                _keyDict.Add(keyTag, key);
+                key.name = data.Tag;
+                key.transform.position = new Vector3(28 * data.Pos - 14, 0.02f, -50f);
+                _keyDict.Add(data.Tag, key);
             }
-            else if (state == InputState.Up && _keyDict.TryGetValue(keyTag, out var key))
+            else if (data.State == InputState.Up && _keyDict.TryGetValue(data.Tag, out var key))
             {
-                _keyDict.Remove(keyTag);
+                _keyDict.Remove(data.Tag);
                 Destroy(key);
             }
         }
+#if UNITY_EDITOR
+        Profiler.EndSample();
+#endif
     }
 
     
