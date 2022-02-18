e47fc5e6a2eeb2c840c717e3fb275107295ca2ba
DDs_moe <1296873979@qq.com>
2022-02-18 23:27:34 +0800

    Add ChartEditorScene

--- Cyan-Stars/Assets/Scripts/ChartEditor/LoadChart.cs
+++ Cyan-Stars/Assets/Scripts/ChartEditor/LoadChart.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json; //JSON .NET For Unity插件
 
-public class CreateNewChart : MonoBehaviour
+public class LoadChart : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-    //按下按钮后创建一个新谱面
-    public void CreateAChart()
+
+    //按下按钮后加载一张谱面
+    public void LoadAChart()
     {
 
     }
