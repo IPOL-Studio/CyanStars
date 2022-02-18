e6c69326657d71701b5d56e4fe322f971b2ef396
YBR <www_ybr_2_com@163.com>
2022-02-19 03:29:53 +0800

    Upload
    Time:2022/2/19/3:29
    简单打音符:)

---
+++ Cyan-Stars/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;//游戏的实例
+    public static GameManager Instance//代理模式
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = Transform.FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+    }
+    [Header("-----游戏数据-----")]
+    [Header("1.Combo数量")]
+    public int combo = 0;//Combo数量
+}
+//This code is writed by Ybr.
