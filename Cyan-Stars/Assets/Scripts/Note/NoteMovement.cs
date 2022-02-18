bdc4b0b78dc5188156bc0142c6807cd1d7bdcbf2
YBR <www_ybr_2_com@163.com>
2022-02-19 00:20:15 +0800

    Upload
    Time:2022/2/19/0:19
    添加了Note的特效

--- Cyan-Stars/Assets/Scripts/Note/NoteMovement.cs
+++ Cyan-Stars/Assets/Scripts/Note/NoteMovement.cs
@@ -11,17 +11,20 @@ public class NoteMovement : MonoBehaviour//音符块的移动代码
     {
         rb = GetComponent<Rigidbody>();//获取音符块的刚体组件
     }
+    public void DestoryEffect()//激活音符块的特效
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
     void Update()//每帧执行
     {
-        //TODO:设置消失特效
         if(rb.position.z < Gamesetting.Instance.noteDisappearZ)//如果音符块的z坐标小于消失点z坐标
         {
-            Destroy(gameObject);//销毁音符块
+            //TODO:只有点到才有特效
+            DestoryEffect();//激活音符块的特效
+            Destroy(gameObject,2f);//特效结束后销毁音符块
         }
-        else
-        {
-            rb.velocity = new Vector3(0, 0, -speed);//设置音符块的移动速度
-        }
+        rb.velocity = new Vector3(0, 0, -speed);//设置音符块的移动速度
     }
 }
 //This code is writed by Ybr.
--- Cyan-Stars/Assets/TextMesh Pro/Examples & Extras/Scripts/TMP_TextInfoDebugTool.cs
+++ Cyan-Stars/Assets/TextMesh Pro/Examples & Extras/Scripts/TMP_TextInfoDebugTool.cs
@@ -10,7 +10,6 @@ namespace TMPro.Examples
     public class TMP_TextInfoDebugTool : MonoBehaviour
     {
         // Since this script is used for debugging, we exclude it from builds.
-        // TODO: Rework this script to make it into an editor utility.
         #if UNITY_EDITOR
         public bool ShowCharacters;
         public bool ShowWords;
