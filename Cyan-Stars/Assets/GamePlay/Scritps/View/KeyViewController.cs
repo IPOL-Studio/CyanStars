331d0c57015be4a37cc1db0ffb11c9a52b04e1a2
YBR <www_ybr_2_com@163.com>
2022-03-01 19:31:46 +0800

    修了一个历史遗留Bug

--- Cyan-Stars/Assets/GamePlay/Scritps/View/KeyViewController.cs
+++ Cyan-Stars/Assets/GamePlay/Scritps/View/KeyViewController.cs
@@ -6,18 +6,21 @@ public class KeyViewController : MonoBehaviour
 {
     [Header("Key预制体")]
     public GameObject keyPrefab;
-    private Dictionary<KeyCode,GameObject> keyList = new Dictionary<KeyCode, GameObject>();
+    public Dictionary<KeyCode,GameObject> keyList = new Dictionary<KeyCode, GameObject>();
     public void keyDown(InputMapData.Item item)
     {
+        /*
         Transform tf = new GameObject().transform;
         tf.position = new Vector3(item.RangeMin * 10.2f - 13.66f,0,0);
         tf.localScale = new Vector3((item.RangeMax - item.RangeMin)*10, 1, 1);
+        */
         if(!keyList.ContainsKey(item.key))
         {
-            GameObject key = Instantiate(keyPrefab, tf);
-            key.transform.parent.SetParent(transform);
+            GameObject key = Instantiate(keyPrefab);
+            key.transform.position = new Vector3(item.RangeMin * 10.2f - 13.66f,0,0);
+            key.transform.localScale = new Vector3((item.RangeMax - item.RangeMin)*10, 0.1f, 115);
+            key.transform.SetParent(transform);
             key.name = item.key.ToString();
-            key.transform.parent.gameObject.name = item.key.ToString();
             keyList[item.key] = key;
         }
         else
