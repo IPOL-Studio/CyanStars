e47c01d500217430200f4e78ba76e0efe6096b6d
YBR <www_ybr_2_com@163.com>
2022-03-05 19:33:02 +0800

    铺面数据全删了，加了一些注释

--- Cyan-Stars/Assets/GamePlay/Scritps/CameraController.cs
+++ Cyan-Stars/Assets/GamePlay/Scritps/CameraController.cs
@@ -2,39 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController : MonoBehaviour//摄像头控制脚本
 {
-    public static CameraController Instance;
-    Transform oldTransform;
-    public List<Vector3> cameraPositions;
-    public List<Vector3> cameraRotations;
-    public List<float> cameraDTimes;
-    public List<SmoothFuncationType> cameraSmoothTypes;
-    public bool onMove = false;
-    int i = 0;
-    void Update()
+    public static CameraController Instance;//单例模式
+    Transform oldTransform;//旧的位置
+    private bool onMove = false;//是否在移动
+    public void moveCamera(Vector3 newPos, Vector3 newRot, float dTime,SmoothFuncationType type = SmoothFuncationType.Linear)//移动摄像头
     {
-        if(!onMove && i < cameraPositions.Count)
-        {
-            moveCamera(cameraPositions[i],cameraRotations[i],cameraDTimes[i],cameraSmoothTypes[i]);
-            i++;
-        }
+        oldTransform = transform;//记录旧的位置
+        StartCoroutine(moveCameraCoroutine(newPos, newRot, dTime, type));//开启协程
     }
-    public void moveCamera(Vector3 newPos, Vector3 newRot, float dTime,SmoothFuncationType type = SmoothFuncationType.Linear)
+    IEnumerator moveCameraCoroutine(Vector3 newPos,Vector3 newRot,float dtime,SmoothFuncationType type)//移动摄像头协程
     {
-        oldTransform = transform;
-        StartCoroutine(moveCameraCoroutine(newPos, newRot, dTime, type));
-    }
-    IEnumerator moveCameraCoroutine(Vector3 newPos,Vector3 newRot,float dtime,SmoothFuncationType type)
-    {
-        Vector3 oldPos = oldTransform.position;
-        Vector3 oldRot = oldTransform.localEulerAngles;
-        float timer = 0;
-        onMove = true;
+        Vector3 oldPos = oldTransform.position;//记录旧的位置
+        Vector3 oldRot = oldTransform.localEulerAngles;//记录旧的角度
+        float timer = 0;//计时器
+        onMove = true;//开始移动
         while(timer <= dtime)
         {
-            timer += Time.deltaTime;
-            switch (type)
+            timer += Time.deltaTime * 1000;//计时器加上时间
+            switch (type)//缓动
             {
                 case SmoothFuncationType.Linear:
                     transform.position = LinearFunction(oldPos, newPos,timer,dtime);
@@ -60,27 +47,27 @@ public class CameraController : MonoBehaviour
             }
             yield return null;
         }
-        onMove = false;
+        onMove = false;//结束移动
         yield break;
     }
     //t:当前时间，d:持续时间 b:开始值 e:结束值
-    private Vector3 LinearFunction(Vector3 b,Vector3 e,float t,float dt)
+    private Vector3 LinearFunction(Vector3 b,Vector3 e,float t,float dt)//线性缓动
     {
         return b + (e - b) * t / dt;
     }
-    private Vector3 SinFunctionEaseIn(Vector3 b, Vector3 e, float t, float dt)
+    private Vector3 SinFunctionEaseIn(Vector3 b, Vector3 e, float t, float dt)//正弦曲线的缓动（sin(t)）/ 从0开始加速的缓动，也就是先慢后快
     {
     return -(e - b) * Mathf.Cos(t/dt * (Mathf.PI/2)) + (e - b) + b;
     }
-    private Vector3 SinFunctionEaseOut(Vector3 b, Vector3 e, float t, float dt)
+    private Vector3 SinFunctionEaseOut(Vector3 b, Vector3 e, float t, float dt)//正弦曲线的缓动（sin(t)）/ 减速到0的缓动，也就是先快后慢
     {
         return (e - b) * Mathf.Sin(t / dt * (Mathf.PI / 2)) + b;
     }
-    private Vector3 SinFunctionEaseInOut(Vector3 b, Vector3 e, float t, float dt)
+    private Vector3 SinFunctionEaseInOut(Vector3 b, Vector3 e, float t, float dt)//正弦曲线的缓动（sin(t)）/ 前半段从0开始加速，后半段减速到0的缓动
     {
         return -(e - b) / 2 * (Mathf.Cos(Mathf.PI * t / dt) - 1) + b;
     }
-    private Vector3 BackEaseIn(Vector3 b, Vector3 e, float t, float dt)
+    private Vector3 BackEaseIn(Vector3 b, Vector3 e, float t, float dt)//超过范围的三次方缓动（(s+1)*t^3 – s*t^2）/ 从0开始加速的缓动，也就是先慢后快
     {
         float s = 1.70158f;
         return (e - b) * (t /= dt) * t * ((s + 1) * t - s) + b;
