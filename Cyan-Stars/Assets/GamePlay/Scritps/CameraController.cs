8102cbad43157d23123d50927b503e653c638e5d
ChenLuan <1296873979@qq.com>
2022-03-09 23:12:55 +0800

    Update

--- Cyan-Stars/Assets/GamePlay/Scritps/CameraController.cs
+++ Cyan-Stars/Assets/GamePlay/Scritps/CameraController.cs
@@ -17,15 +17,14 @@ public class CameraController : MonoBehaviour//摄像头控制脚本
     Transform oldTransform;//旧的位置
     private bool onMove = false;//是否在移动
     public Vector3 defaultPosition;//默认位置
-    private static object ojb = new object();
     void Awake()
     {
         Instance = this;//单例模式
         transform.position = defaultPosition;//设置默认位置
     }
-    public void MoveCamera(Vector3 newPos, Vector3 newRot, float dTime,SmoothFuncationType type)//移动摄像头
+    public void MoveCamera(Vector3 newPos, Vector3 newRot, float dTime,SmoothFuncationType type = SmoothFuncationType.Linear)//移动摄像头
     {
-        if (onMove) return;
+        if(onMove)return;
         oldTransform = transform;//记录旧的位置
         StartCoroutine(MoveCameraCoroutine(newPos, newRot, dTime, type));//开启协程
     }
@@ -36,14 +35,14 @@ public class CameraController : MonoBehaviour//摄像头控制脚本
         Vector3 oldRot = oldTransform.localEulerAngles;//记录旧的角度
         float timer = 0;//计时器
         onMove = true;//开始移动
-        while (timer <= dtime)
+        while(timer <= dtime)
         {
             timer += Time.deltaTime * 1000;//计时器加上时间(ms)
             switch (type)//缓动
             {
                 case SmoothFuncationType.Linear:
-                    transform.position = LinearFunction(oldPos, newPos, timer, dtime);
-                    transform.localEulerAngles = LinearFunction(oldRot, newRot, timer, dtime);
+                    transform.position = LinearFunction(oldPos, newPos,timer,dtime);
+                    transform.localEulerAngles = LinearFunction(oldRot, newRot,timer,dtime);
                     break;
                 case SmoothFuncationType.SineaseIn:
                     transform.position = SinFunctionEaseIn(oldPos, newPos, timer, dtime);
@@ -75,7 +74,7 @@ public class CameraController : MonoBehaviour//摄像头控制脚本
     }
     private Vector3 SinFunctionEaseIn(Vector3 b, Vector3 e, float t, float dt)//正弦曲线的缓动（sin(t)）/ 从0开始加速的缓动，也就是先慢后快
     {
-        return -(e - b) * Mathf.Cos(t/dt * (Mathf.PI/2)) + (e - b) + b;
+    return -(e - b) * Mathf.Cos(t/dt * (Mathf.PI/2)) + (e - b) + b;
     }
     private Vector3 SinFunctionEaseOut(Vector3 b, Vector3 e, float t, float dt)//正弦曲线的缓动（sin(t)）/ 减速到0的缓动，也就是先快后慢
     {
@@ -87,7 +86,7 @@ public class CameraController : MonoBehaviour//摄像头控制脚本
     }
     private Vector3 BackEaseIn(Vector3 b, Vector3 e, float t, float dt)//超过范围的三次方缓动（(s+1)*t^3 – s*t^2）/ 从0开始加速的缓动，也就是先慢后快
     {
-        float s = 0f;
+        float s = 1.70158f;
         return (e - b) * (t /= dt) * t * ((s + 1) * t - s) + b;
     }
 }
