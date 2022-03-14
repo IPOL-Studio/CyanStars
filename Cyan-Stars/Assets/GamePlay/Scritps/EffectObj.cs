c14e6faf6064d05415e782879a64173075f02f09
YBR <www_ybr_2_com@163.com>
2022-03-14 23:23:45 +0800

    修复Frame渐入&添加粒子渐隐

--- Cyan-Stars/Assets/GamePlay/Scritps/EffectObj.cs
+++ Cyan-Stars/Assets/GamePlay/Scritps/EffectObj.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class EffectObj : MonoBehaviour
 {
     public float destroyTime = -1;
+    public VisualEffect visualEffect;
+    float visualEffectStartCount;
 
     void Start()
     {
+        visualEffect = GetComponent<VisualEffect>();
+        visualEffectStartCount = visualEffect.GetFloat("Count");
         if(destroyTime > 0)
         {
-            Destroy(gameObject, destroyTime);
+            StartCoroutine(DestroySelf());
         }
+    }
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds((destroyTime - 3000f)/1000);
+        for(int i = 200;i > 0;i --)
+        {
+            visualEffect.SetFloat("Count",(i/2f)/100f * visualEffectStartCount);
+            yield return new WaitForSeconds(0.01f);
+        }
+        Destroy(gameObject,1);
     }
 }
