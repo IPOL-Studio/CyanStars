34099d4e333e2ecfdb99a066aa317a99006fb46f
CL <62653664+Chen-Luan@users.noreply.github.com>
2022-03-25 21:42:07 +0800

    Update GameManager.cs

--- Cyan-Stars/Assets/GamePlay/Scritps/OldScritps/GameManager.cs
+++ Cyan-Stars/Assets/GamePlay/Scritps/OldScritps/GameManager.cs
@@ -40,7 +40,7 @@ public class GameManager : MonoBehaviour
     public int missNum = 0;
     [Header("8.当前歌词")] 
     public string curLrcText;
-    [Header("10.全谱总分")]
+    [Header("9.全谱总分")]
     public float fullScore = 0;
     
     [Header("-----游戏模式-----")]
