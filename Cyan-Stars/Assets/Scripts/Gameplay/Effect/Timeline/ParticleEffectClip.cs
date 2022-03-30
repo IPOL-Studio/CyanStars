032651f0729192949f29980bcf6a6c168ee810b1
CatImmortal <39085172+CatImmortal@users.noreply.github.com>
2022-03-30 22:58:32 +0800

    timeline接入特效控制 (#62)

---
+++ Cyan-Stars/Assets/Scripts/Gameplay/Effect/Timeline/ParticleEffectClip.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using CatTimeline;
+using UnityEngine;
+
+/// <summary>
+/// 粒子特效片段
+/// </summary>
+public class ParticleEffectClip : BaseClip<EffectTrack>
+{
+    /// <summary>
+    /// 粒子特效预制索引
+    /// </summary>
+    private int effectPrefabIndex;
+
+    private Vector3 position;
+    private Vector3 rotation;
+    
+    /// <summary>
+    /// 粒子数量，小于等于0则使用默认值
+    /// </summary>
+    private int particleCount;
+
+    /// <summary>
+    /// 持续时间
+    /// </summary>
+    private float duration;
+    
+    public ParticleEffectClip(float startTime, float endTime, EffectTrack owner, int effectPrefabIndex, Vector3 position, Vector3 rotation, int particleCount, float duration) : base(startTime, endTime, owner)
+    {
+        this.effectPrefabIndex = effectPrefabIndex;
+        this.position = position;
+        this.rotation = rotation;
+        this.particleCount = particleCount;
+        this.duration = duration;
+    }
+
+    public override void OnEnter()
+    {
+        GameObject effectObj = Object.Instantiate(Owner.EffectGOs[effectPrefabIndex], position, Quaternion.Euler(rotation));
+        effectObj.gameObject.transform.SetParent(Owner.EffectParent);
+
+        EffectObj eo = effectObj.GetComponent<EffectObj>();
+        eo.destroyTime = duration;
+        eo.visualEffectStartCount = particleCount;
+    }
+}
