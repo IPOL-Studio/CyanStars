51af442d9641853487e2735a3b6fdfec910a6ba0
YBR <www_ybr_2_com@163.com>
2022-04-06 13:34:35 +0800

    整理DragNote的代码

--- Cyan-Stars/Assets/Scripts/Gameplay/Note/Logic/DragNote.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/Note/Logic/DragNote.cs
@@ -21,29 +21,37 @@ namespace CyanStars.Gameplay.Note
         {
             base.OnUpdate(deltaTime, noteSpeedRate);
 
-            if (CanReceiveInput() && GameManager.Instance.isAutoMode && !isHit)
+            if (isHit && LogicTimer <= 0)//接住并过线
             {
-                viewObject.CreateEffectObj(data.Width);
-                GameManager.Instance.maxScore++;
-                GameManager.Instance.RefreshData(1, 1, EvaluateType.Exact, 0);
+                DestroySelf(false);//立即销毁
+                return;
+            }
+
+            if (LogicTimer < -EvaluateHelper.DragTimeRange)//没接住Miss
+            {
+                DestroySelf();//延迟销毁
+
+                LogHelper.NoteLogger.Log(new DefaultNoteJudgeLogArgs(data, EvaluateType.Miss));//Log
+
+                GameManager.Instance.maxScore += data.GetFullScore();//更新最理论高分
+                GameManager.Instance.RefreshData(-1, -1, EvaluateType.Miss, float.MaxValue);//更新数据
+            }
+        }
+
+        public override void OnUpdateInAutoMode(float deltaTime, float noteSpeedRate)
+        {
+            base.OnUpdateInAutoMode(deltaTime, noteSpeedRate);
+
+            if (CanReceiveInput() && !isHit)
+            {
+                viewObject.CreateEffectObj(data.Width);//生成特效
+                
+                GameManager.Instance.maxScore += data.GetFullScore();//更新理论最高分
+                GameManager.Instance.RefreshData(1, 0.25f, EvaluateType.Exact, 0);//更新数据
+                /*
+                *Drag的最高分为0.25，所以不能用EvaluateHelper.GetScoreWithEvaluate获取分数
+                */
                 isHit = true;
-                return;
-            }
-
-            if (isHit && LogicTimer <= 0)
-            {
-                DestroySelf(false);
-                return;
-            }
-
-            if (LogicTimer < -EvaluateHelper.DragTimeRange)
-            {
-                //没接住 miss
-                DestroySelf();
-                //Debug.LogError($"Drag音符miss：{data}");
-                LogHelper.NoteLogger.Log(new DefaultNoteJudgeLogArgs(data, EvaluateType.Miss));
-                GameManager.Instance.maxScore++;
-                GameManager.Instance.RefreshData(-1, -1, EvaluateType.Miss, float.MaxValue);
             }
         }
 
@@ -51,33 +59,28 @@ namespace CyanStars.Gameplay.Note
         {
             base.OnInput(inputType);
 
-            switch (inputType)
+            if (inputType != InputType.Press) return;//只处理按下的情况
+
+            if (isHit)return;//已经接住了
+
+            viewObject.CreateEffectObj(data.Width);//生成特效
+            LogHelper.NoteLogger.Log(new DefaultNoteJudgeLogArgs(data, EvaluateType.Exact));//Log
+
+            GameManager.Instance.maxScore += data.GetFullScore();//更新理论最高分
+            GameManager.Instance.RefreshData(1, 0.25f, EvaluateType.Exact, float.MaxValue);//更新数据
+            /*
+             *Drag的最高分为0.25，所以不能用EvaluateHelper.GetScoreWithEvaluate获取分数
+            */
+
+            if (LogicTimer > 0)
             {
-
-                case InputType.Press:
-
-                    if (isHit)
-                    {
-                        return;
-                    }
-
-                    viewObject.CreateEffectObj(data.Width);
-                    //Debug.LogError($"Drag音符命中：{data}");
-                    LogHelper.NoteLogger.Log(new DefaultNoteJudgeLogArgs(data, EvaluateType.Exact));
-                    GameManager.Instance.maxScore++;
-                    GameManager.Instance.RefreshData(1, 1, EvaluateType.Exact, float.MaxValue);
-                    if (LogicTimer > 0)
-                    {
-                        //早按准点放
-                        isHit = true;
-                    }
-                    else
-                    {
-                        //晚按即刻放
-                        DestroySelf(false);
-                    }
-
-                    break;
+                //早按准点放
+                isHit = true;
+            }
+            else
+            {
+                //晚按即刻放
+                DestroySelf(false);
             }
         }
     }
