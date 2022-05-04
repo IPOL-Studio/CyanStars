6e25f1b621f2dabfec3be586450ccaa5e24b93c3
CatImmortal <1304085906@qq.com>
2022-05-04 21:24:26 +0800

    实现UIManager

---
+++ Cyan-Stars/Assets/Scripts/Framework/UI/UIDataAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CyanStars.Framework.UI
+{
+    /// <summary>
+    /// UI数据特性
+    /// </summary>
+    public class UIDataAttribute : Attribute
+    {
+        /// <summary>
+        /// UI预制体名
+        /// </summary>
+        public string UIPrefabName;
+        
+        /// <summary>
+        /// UI组名
+        /// </summary>
+        public string UIGroupName;
+        
+
+        public UIDataAttribute(string uiPrefabName, string uiGroupName)
+        {
+            UIPrefabName = uiPrefabName;
+            UIGroupName = uiGroupName;
+        }
+    }
+}
