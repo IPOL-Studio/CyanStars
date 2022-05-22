8673ed0471d67187c8310cb1c4d1cab243174f63
CatImmortal <39085172+CatImmortal@users.noreply.github.com>
2022-05-22 16:25:24 +0800

    框架开发 (#103)
    * 游戏对象池
    
    1.为不同对象池增加子节点作为Root
    2.对象池使用方式改为基于预制体名和基于游戏对象模板的两种
    3.增加对长时间未使用的对象池的销毁（仅限基于预制体名的）
    
    * 增加UIItem基类与相关API

--- Cyan-Stars/Assets/Scripts/Framework/UI/BaseUIItem.cs
+++ Cyan-Stars/Assets/Scripts/Framework/UI/BaseUIItem.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace CyanStars.Framework.UI
@@ -7,9 +6,59 @@ namespace CyanStars.Framework.UI
     /// <summary>
     /// UIItem基类
     /// </summary>
-    public abstract class BaseUIItem
+    public abstract class BaseUIItem : MonoBehaviour
     {
+        [NonSerialized]
+        public string PrefabName;
+        
+        [NonSerialized]
+        public GameObject Template;
+        
+        private void Awake()
+        {
+            OnCreate();
+        }
 
+        /// <summary>
+        /// 创建UIItem时调用
+        /// </summary>
+        public virtual void OnCreate()
+        {
+            
+        }
+
+        /// <summary>
+        /// 显示UIItem时调用
+        /// </summary>
+        public virtual void OnShow()
+        {
+            
+        }
+
+        /// <summary>
+        /// 隐藏UIItem时调用
+        /// </summary>
+        public virtual void OnHide()
+        {
+            
+        }
+
+        /// <summary>
+        /// 销毁UIItem时调用
+        /// </summary>
+        public virtual void OnDestroy()
+        {
+            
+        }
+
+        
+        /// <summary>
+        /// 刷新Item
+        /// </summary>
+        public virtual void RefreshItem<T>(T data) where T : UIItemData
+        {
+            
+        }
     }
 }
 
