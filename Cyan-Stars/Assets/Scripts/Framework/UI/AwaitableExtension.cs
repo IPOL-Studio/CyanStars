489976ed9d0dda45b1ac5d2ac45597a47e5c6f75
CatImmortal <39085172+CatImmortal@users.noreply.github.com>
2022-05-26 00:34:13 +0800

    框架开发 (#109)
    * 将UIItem的相关API移动到UIManager中
    
    * 修改UIGroup的api

--- Cyan-Stars/Assets/Scripts/Framework/UI/AwaitableExtension.cs
+++ Cyan-Stars/Assets/Scripts/Framework/UI/AwaitableExtension.cs
@@ -11,17 +11,17 @@ namespace CyanStars.Framework.UI
         /// <summary>
         /// 打开UI（可等待）
         /// </summary>
-        public static Task<T> AwaitOpenUI<T>(this UIManager self) where T : BaseUIPanel
+        public static Task<T> AwaitOpenUIPanel<T>(this UIManager self) where T : BaseUIPanel
         {
             TaskCompletionSource<T> tcs = new TaskCompletionSource<T>();
-            self.OpenUI<T>(uiPanel => tcs.SetResult(uiPanel));
+            self.OpenUIPanel<T>(uiPanel => tcs.SetResult(uiPanel));
             return tcs.Task;
         }
 
         /// <summary>
         /// 使用预制体名获取UIItem（可等待）
         /// </summary>
-        public static Task<T> AwaitGetUIItem<T>(this BaseUIPanel self, string prefabName, Transform parent) where T : BaseUIItem
+        public static Task<T> AwaitGetUIItem<T>(this UIManager self, string prefabName, Transform parent) where T : BaseUIItem
         {
             TaskCompletionSource<T> tcs = new TaskCompletionSource<T>();
             self.GetUIItem<T>(prefabName, parent, item => tcs.SetResult(item));
@@ -31,7 +31,7 @@ namespace CyanStars.Framework.UI
         /// <summary>
         /// 使用模板获取UIItem（可等待）
         /// </summary>
-        public static Task<T> AwaitGetUIItem<T>(this BaseUIPanel self, GameObject template, Transform parent) where T : BaseUIItem
+        public static Task<T> AwaitGetUIItem<T>(this UIManager self, GameObject template, Transform parent) where T : BaseUIItem
         {
             TaskCompletionSource<T> tcs = new TaskCompletionSource<T>();
             self.GetUIItem<T>(template, parent, item => tcs.SetResult(item));
