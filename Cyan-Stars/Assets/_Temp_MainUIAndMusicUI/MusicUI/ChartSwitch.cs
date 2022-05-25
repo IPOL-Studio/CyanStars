fc1f5fc4cdb77a1f2fbd03e5cc869ffe417be5bb
CL <62653664+Chen-Luan@users.noreply.github.com>
2022-05-25 20:04:49 +0800

    改UI (#108)
    * 修正一个小BUG
    
    * 修改临时文件目录
    
    * 添加Lv显示
    
    * 添加Load动画（测试）

---
+++ Cyan-Stars/Assets/_Temp_MainUIAndMusicUI/MusicUI/ChartSwitch.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//未完成，你可以继续编下去
+//这个脚本用于官方曲目的谱面切换
+//按下Chart0/1/2/3按钮后，读取下一个难度的数据，隐藏当前按钮，轮换显示下一个按钮，同时切换StartButton的Lv
+
+public class ChartSwitch : MonoBehaviour
+{
+    void ChangeChart()
+    {
+
+    }
+}
