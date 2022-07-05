931b49def57178e23857b604089446d847333e84
CatImmortal <1304085906@qq.com>
2022-07-06 01:42:09 +0800

    增加移动端的输入检测支持

---
+++ Cyan-Stars/Assets/Scripts/Framework/ReferencePool/ReferencePool.Pool.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CyanStars.Framework.RefrencePool
+{
+    public static partial class ReferencePool
+    {
+        private class Pool
+        {
+            private readonly List<IReference> list = new List<IReference>();
+
+            public T Get<T>() where T : IReference,new ()
+            {
+                if (list.Count == 0)
+                {
+                    T obj = new T();
+                    //Debug.Log($"创建引用:{obj.GetType()}");
+                    return obj;
+                }
+
+                IReference reference = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+
+                //Debug.Log($"获取引用:{typeof(T).Name}");
+                return (T) reference;
+            }
+
+            public void Release(IReference reference)
+            {
+                //Debug.Log($"归还引用:{reference.GetType()}");
+                reference.Clear();
+                list.Add(reference);
+            }
+        }
+    }
+
+}
---
+++ Cyan-Stars/Assets/Scripts/Gameplay/Event/EmptyEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CyanStars.Framework.RefrencePool;
+
+namespace CyanStars.Gameplay.Event
+{
+    public class EmptyEventArgs : EventArgs,IReference
+    {
+        public static EmptyEventArgs Create()
+        {
+            EmptyEventArgs eventArgs = ReferencePool.Get<EmptyEventArgs>();
+            return eventArgs;
+        }
+
+        public void Clear()
+        {
+
+        }
+    }
+}
