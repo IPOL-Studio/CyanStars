21d9eaf6a816402925e0cb6d95b71833b596dcc8
memorydream <memorydream.chen@outlook.com>
2023-04-02 02:14:51 +0800

    fix bug and support command field type parameter

--- Cyan-Stars/Assets/Scripts/Libraries/MunNovel/MunNovel.Core/Utils/CommandUtils.cs
+++ Cyan-Stars/Assets/Scripts/Libraries/MunNovel/MunNovel.Core/Utils/CommandUtils.cs
@@ -36,6 +36,13 @@ namespace MunNovel.Utils
                 };
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="commandType"></param>
+        /// <returns>paramater name map to parameter metadatas or empty dict(not null)</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         public static ReadOnlyDictionary<string, CommandParameterMetadata> GetCommandParameters(Type commandType)
         {
             if (commandType == null)
@@ -57,11 +64,22 @@ namespace MunNovel.Utils
                 GetPropertyCommandParameter(props, dict);
             }
 
+            var fields = commandType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetField);
+            if (fields != null && fields.Length > 0)
+            {
+                if (dict == null)
+                    dict = new Dictionary<string, CommandParameterMetadata>(fields.Length);
+                GetFieldCommandParameter(fields, dict);
+            }
+
             return dict == null
                 ? EmptyCommandPropMetadata
                 : new ReadOnlyDictionary<string, CommandParameterMetadata>(dict);
         }
 
+        // 获取以 property 形式声明的 parameters
+        // property 需要具有 set 访问器(可写)
+        // property 需要 [CommandParameterAttribute]，或 set 为 public
         private static void GetPropertyCommandParameter(PropertyInfo[] props, Dictionary<string, CommandParameterMetadata> dict)
         {
             for (int i = 0; i < props.Length; i++)
@@ -73,14 +91,28 @@ namespace MunNovel.Utils
                 }
 
                 // 没有CommandParameterAttribute做标记，并且set不是public的，跳过
-                if (!props[i].IsDefined(typeof(CommandParameterAttribute), false) && 
-                    props[i].GetSetMethod(true) == null)
+                if (!props[i].IsDefined(typeof(CommandParameterAttribute), false) && // isDefinedAttr
+                    props[i].GetSetMethod(false) == null)                            // isPublicSetter
                 {
                     continue;
                 }
 
                 var metadata = new CommandParameterMetadata(props[i]);
                 dict.Add(metadata.Name, metadata);
+            }
+        }
+
+        // 获取以 field 形式声明的 parameters
+        // field 需要 [CommandParameterAttribute]，且非 readonly
+        private static void GetFieldCommandParameter(FieldInfo[] fields,  Dictionary<string, CommandParameterMetadata> dict)
+        {
+            for (int i = 0; i < fields.Length; i++)
+            {
+                if (fields[i].IsDefined(typeof(CommandParameterAttribute), false) && !fields[i].IsInitOnly)
+                {
+                    var metadata = new CommandParameterMetadata(fields[i]);
+                    dict.Add(metadata.Name, metadata);
+                }
             }
         }
 
