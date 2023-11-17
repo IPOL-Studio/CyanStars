26bc9a45de78fe2c3d6370c40dafe343f8226634
memorydream <memorydream.chen@outlook.com>
2023-11-17 10:14:46 +0800

    fix: Unity console exception log result (#192)
    * remove repeat LogLevel in LogEntry
    
    * fix: Unity console exception result
    
    * add space
    
    * Update UnityDebugExceptionFormatter.cs
    
    * fix: format

--- Cyan-Stars/Assets/Scripts/Framework/Logging/LogEntry.cs
+++ Cyan-Stars/Assets/Scripts/Framework/Logging/LogEntry.cs
@@ -4,14 +4,12 @@ namespace CyanStars.Framework.Logging
 {
     public readonly struct LogEntry<T>
     {
-        public readonly LogLevel LogLevel;
         public readonly DateTime Timestamp;
         public readonly T State;
         public readonly UnityEngine.Object Context;
 
-        public LogEntry(LogLevel logLevel, DateTime timestamp, T state, UnityEngine.Object context = null)
+        public LogEntry(DateTime timestamp, T state, UnityEngine.Object context = null)
         {
-            LogLevel  = logLevel;
             Timestamp = timestamp;
             State     = state;
             Context   = context;
