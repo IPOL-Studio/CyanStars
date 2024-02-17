6520f376abfb1b04b703c3e5001c81dff07be6d1
YbrStudio <79822594+Ybr2007@users.noreply.github.com>
2024-02-17 22:02:50 +0800

    实现Hold判定时在判定线上的截断

--- Cyan-Stars/Assets/Scripts/Gameplay/MusicGame/Note/View/HoldViewObject.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/MusicGame/Note/View/HoldViewObject.cs
@@ -11,6 +11,44 @@ namespace CyanStars.Gameplay.MusicGame
 
         private MaterialPropertyBlock block = null;
 
+        /// <summary>
+        /// 是否被按下（在判定线上截断）
+        /// </summary>
+        public bool Pressed = false;
+
+        /// <summary>
+        /// 记录上一次截断时Hold的实际的Distance，在松开时将distance减去这个值修正位置
+        /// </summary>
+        private float lastDistanceWhenPressed = 0;
+
+
+        public void SetPressed(bool pressed)
+        {
+            this.Pressed = pressed;
+            OnUpdate(ViewDistance);
+        }
+
+        public override void OnUpdate(float viewDistance)
+        {
+            ViewDeltaTime = this.ViewDistance - viewDistance;
+            this.ViewDistance = viewDistance;
+            Vector3 pos = transform.position;
+
+            if (Pressed)
+            {
+                // 视觉上的distance被设置为0（截断）
+                pos.z = viewDistance > 0 ? ViewDistance : 0;
+                lastDistanceWhenPressed = viewDistance;
+            }
+            else
+            {
+                // 按下时实际的distance为lastDistanceWhenPressed，减去这个值修正
+                pos.z = viewDistance - lastDistanceWhenPressed;
+            }
+
+            transform.position = pos;
+        }
+
         public void SetLength(float length)
         {
             var t = transform;
@@ -26,7 +64,6 @@ namespace CyanStars.Gameplay.MusicGame
             block.SetFloat(Flicker, 0);
             meshRenderer.SetPropertyBlock(block);
         }
-
 
 
         private void OnEnable()
@@ -45,6 +82,14 @@ namespace CyanStars.Gameplay.MusicGame
             {
                 this.meshRenderer.SetPropertyBlock(block);
             }
+        }
+
+        public override void DestroySelf(bool autoMove = true)
+        {
+            // 将对象归还到对象池之前重置为未按下状态
+            Pressed = false;
+            lastDistanceWhenPressed = 0;
+            base.DestroySelf(autoMove);
         }
     }
 }
