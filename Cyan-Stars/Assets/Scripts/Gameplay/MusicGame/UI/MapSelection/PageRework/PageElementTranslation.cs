82ec7597e10c7e4574f7f0f32e6fedc99cb978c2
memorydream <memorydream.chen@outlook.com>
2024-06-01 21:05:19 +0800

    fix: translation

--- Cyan-Stars/Assets/Scripts/Gameplay/MusicGame/UI/MapSelection/PageRework/PageElementTranslation.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/MusicGame/UI/MapSelection/PageRework/PageElementTranslation.cs
@@ -5,17 +5,17 @@ namespace CyanStars.Gameplay.MusicGame
 {
     public class PageElementTranslation : MonoBehaviour, IPageElementAnimation
     {
-        private Vector3 defaultPos;
+        private Vector2 defaultPos;
         private RectTransform rectTransform;
 
-        public Vector3 RelativeExitTargetPos;
+        public Vector2 RelativeExitTargetPos;
 
         private Tween runningTween;
 
         private void Awake()
         {
             rectTransform = transform as RectTransform;
-            defaultPos = rectTransform.localPosition;
+            defaultPos = rectTransform.anchoredPosition;
         }
 
         public void OnEnter(MapSelectionPageChangeArgs args)
@@ -23,8 +23,8 @@ namespace CyanStars.Gameplay.MusicGame
             if (runningTween?.IsPlaying() ?? false)
                 runningTween.Kill();
 
-            rectTransform.localPosition = defaultPos + RelativeExitTargetPos;
-            runningTween = rectTransform.DOLocalMove(defaultPos, args.FadeTime).SetEase(args.AnimationEase);
+            rectTransform.anchoredPosition = defaultPos + RelativeExitTargetPos;
+            runningTween = rectTransform.DOAnchorPos(defaultPos, args.FadeTime).SetEase(args.AnimationEase);
         }
 
         public void OnExit(MapSelectionPageChangeArgs args)
@@ -32,8 +32,8 @@ namespace CyanStars.Gameplay.MusicGame
             if (runningTween?.IsPlaying() ?? false)
                 runningTween.Kill();
 
-            rectTransform.localPosition = defaultPos;
-            runningTween = rectTransform.DOLocalMove(defaultPos + RelativeExitTargetPos, args.FadeTime).SetEase(args.AnimationEase);
+            rectTransform.anchoredPosition = defaultPos;
+            runningTween = rectTransform.DOAnchorPos(defaultPos + RelativeExitTargetPos, args.FadeTime).SetEase(args.AnimationEase);
         }
     }
 }
