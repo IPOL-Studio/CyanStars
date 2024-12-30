b9d4cf5f95f248b473c65d0550fab7cc1873c220
CL <62653664+Chen-Luan@users.noreply.github.com>
2024-12-30 23:25:52 +0800

    feat: 修改音游成绩和难度枚举类
    - Grade 新增说明，玩家从未完成游玩的谱面为null
    - Difficulty 新增“未定义”难度，允许多个存在谱包和编辑器内，但不被游戏读取

--- Cyan-Stars/Assets/Scripts/Gameplay/GameSave/ChartGrade.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/GameSave/ChartGrade.cs
@@ -3,6 +3,9 @@ namespace CyanStars.Gameplay.GameSave
     /// <summary>
     /// 音游谱面成绩评级
     /// </summary>
+    /// <remarks>
+    /// 可为null，代表玩家从未游玩/完成过该谱面
+    /// </remarks>
     public enum ChartGrade
     {
         Clear,
--- Cyan-Stars/Assets/Scripts/Gameplay/MusicGame/MapData/ChartDifficulty.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/MusicGame/MapData/ChartDifficulty.cs
@@ -5,9 +5,10 @@ namespace CyanStars.Gameplay.MusicGame
     /// </summary>
     public enum ChartDifficulty
     {
-        KuiXing = 0, // 窥星（最简单）
-        QiMing = 1, // 启明
-        TianShu = 2, // 天枢
-        WuYin = 3 // 无垠（最难）
+        Undefined = 0, // 未定义的难度，可有多个存在于谱包内，可以被谱面编辑器加载，但不会被游戏加载
+        KuiXing = 1, // 窥星（最简单）
+        QiMing = 2, // 启明
+        TianShu = 3, // 天枢
+        WuYin = 4 // 无垠（最难）
     }
 }
