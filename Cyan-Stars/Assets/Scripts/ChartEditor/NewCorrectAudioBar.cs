697814f79601cbfdff5b930c4d5de42b66bc8bab
CL <62653664+Chen-Luan@users.noreply.github.com>
2025-06-17 10:10:18 +0800

    [WIP] Update: 按钮弹窗

---
+++ Cyan-Stars/Assets/Scripts/ChartEditor/NewCorrectAudioBar.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class NewCorrectAudioBar : MonoBehaviour
+{
+    /// <summary>
+    /// 自动接收 LoopScrollRect 回调
+    /// </summary>
+    /// <remarks>别删，删了会报错</remarks>
+    /// <param name="index">自身元素的序号下标，会在每次进入可视范围时调用</param>
+    void ScrollCellIndex(int index)
+    {
+    }
+}
