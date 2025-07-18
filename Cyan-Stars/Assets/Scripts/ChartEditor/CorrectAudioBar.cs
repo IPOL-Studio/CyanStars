6af3e128b19e46c26518de5f89482fc9d0d164a2
CL <62653664+Chen-Luan@users.noreply.github.com>
2025-07-18 20:47:58 +0800

    Feat: 为无限滚动元素统一 ILoopScrollRectItem 接口

--- Cyan-Stars/Assets/Scripts/ChartEditor/CorrectAudioBar.cs
+++ Cyan-Stars/Assets/Scripts/ChartEditor/CorrectAudioBar.cs
@@ -1,8 +1,9 @@
+using CyanStars.ChartEditor;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CorrectAudioBar : MonoBehaviour
+public class CorrectAudioBar : MonoBehaviour, ILoopScrollRectItem
 {
     public TMP_Text Text;
     public bool EnableDelete;
@@ -13,12 +14,7 @@ public class CorrectAudioBar : MonoBehaviour
         DeleteButton.gameObject.SetActive(EnableDelete);
     }
 
-    /// <summary>
-    /// 自动接收 LoopScrollRect 回调
-    /// </summary>
-    /// <remarks>别删，删了会报错</remarks>
-    /// <param name="index">自身元素的序号下标，会在每次进入可视范围时调用</param>
-    void ScrollCellIndex(int index)
+    public void ScrollCellIndex(int index)
     {
     }
 }
--- Cyan-Stars/Assets/Scripts/ChartEditor/NewCorrectAudioBar.cs
+++ Cyan-Stars/Assets/Scripts/ChartEditor/NewCorrectAudioBar.cs
@@ -1,13 +1,9 @@
+using CyanStars.ChartEditor;
 using UnityEngine;
 
-public class NewCorrectAudioBar : MonoBehaviour
+public class NewCorrectAudioBar : MonoBehaviour, ILoopScrollRectItem
 {
-    /// <summary>
-    /// 自动接收 LoopScrollRect 回调
-    /// </summary>
-    /// <remarks>别删，删了会报错</remarks>
-    /// <param name="index">自身元素的序号下标，会在每次进入可视范围时调用</param>
-    void ScrollCellIndex(int index)
+    public void ScrollCellIndex(int index)
     {
     }
 }
