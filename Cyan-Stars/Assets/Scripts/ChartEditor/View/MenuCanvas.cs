b7816869d9e628b0ca8bdeade5a163f4025023df
CL <62653664+Chen-Luan@users.noreply.github.com>
2025-08-23 14:35:49 +0800

    Add: View/MenuCanvas

---
+++ Cyan-Stars/Assets/Scripts/ChartEditor/View/MenuCanvas.cs
@@ -0,0 +1,48 @@
+using CyanStars.ChartEditor.ViewModel;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CyanStars.ChartEditor.View
+{
+    public enum MenuButtons
+    {
+        Menu,
+        Save,
+        Play
+    }
+
+    /// <summary>
+    /// 左上角菜单 view 层，用于控制按钮
+    /// </summary>
+    public class MenuCanvas : BaseView
+    {
+        [SerializeField]
+        private Button menuButton;
+
+        [SerializeField]
+        private Button saveButton;
+
+        [SerializeField]
+        private Button playButton;
+
+
+        private MainViewModel mainViewModel;
+
+
+        public override void Bind(MainViewModel viewModel)
+        {
+            mainViewModel = viewModel;
+
+            menuButton.onClick.AddListener(() => { mainViewModel.MenuButtonClick(MenuButtons.Menu); });
+            saveButton.onClick.AddListener(() => { mainViewModel.MenuButtonClick(MenuButtons.Save); });
+            playButton.onClick.AddListener(() => { mainViewModel.MenuButtonClick(MenuButtons.Play); });
+        }
+
+        private void OnDestroy()
+        {
+            menuButton.onClick.RemoveAllListeners();
+            saveButton.onClick.RemoveAllListeners();
+            playButton.onClick.RemoveAllListeners();
+        }
+    }
+}
--- Cyan-Stars/Assets/Scripts/ChartEditor/ViewModel/MainViewModel.cs
+++ Cyan-Stars/Assets/Scripts/ChartEditor/ViewModel/MainViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel;
 using CyanStars.ChartEditor.Model;
 using CyanStars.ChartEditor.View;
@@ -26,6 +27,15 @@ namespace CyanStars.ChartEditor.ViewModel
         public void ChangeEditTool(EditTools editTool)
         {
             SelectedEditTool = editTool;
+        }
+
+        /// <summary>
+        /// 当左上角菜单窗口中的一级按钮被点击时
+        /// </summary>
+        public void MenuButtonClick(MenuButtons menuButton)
+        {
+            throw new NotSupportedException();
+            // TODO: 完善点击响应逻辑
         }
 
         // protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
