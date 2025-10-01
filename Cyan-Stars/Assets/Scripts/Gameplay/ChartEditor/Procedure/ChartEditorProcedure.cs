26cc381728b9c7285ba002c1f5128d160ce3ea6a
CL <62653664+Chen-Luan@users.noreply.github.com>
2025-10-01 11:04:12 +0800

    [WIP] Update: 添加制谱器流程、移动场景文件位置、构建资源包

---
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/Procedure/ChartEditorProcedure.cs
@@ -0,0 +1,29 @@
+﻿using CyanStars.Framework;
+using CyanStars.Framework.FSM;
+using UnityEngine.SceneManagement;
+
+namespace CyanStars.GamePlay.ChartEditor.Procedure
+{
+    [ProcedureState]
+    public class ChartEditorProcedure : BaseState
+    {
+        private const string ScenePath = "Assets/BundleRes/Scenes/ChartEditor.unity";
+
+        private Scene scene;
+
+        public override async void OnEnter()
+        {
+            GameRoot.MainCamera.gameObject.SetActive(false);
+            scene = await GameRoot.Asset.LoadSceneAsync(ScenePath);
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+        }
+
+        public override void OnExit()
+        {
+            GameRoot.MainCamera.gameObject.SetActive(true);
+        }
+    }
+}
