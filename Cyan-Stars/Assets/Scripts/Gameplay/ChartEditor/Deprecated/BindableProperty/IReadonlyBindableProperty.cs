a4205a37bd71ed0496b7415730a9c3f66cebe0a1
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-02-11 23:24:51 +0800

    Update: 停用旧代码

--- Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/Deprecated/BindableProperty/IReadonlyBindableProperty.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/Deprecated/BindableProperty/IReadonlyBindableProperty.cs
@@ -1,20 +1,22 @@
-﻿using System;
+﻿// using System;
+//
+// namespace CyanStars.Gameplay.ChartEditor.BindableProperty
+// {
+//     /// <summary>
+//     /// 只读的可观察属性接口
+//     /// </summary>
+//     public interface IReadonlyBindableProperty<out T>
+//     {
+//         /// <summary>
+//         /// 获取当前值
+//         /// </summary>
+//         T Value { get; }
+//
+//         /// <summary>
+//         /// 值变化时触发的事件
+//         /// </summary>
+//         event Action<T> OnValueChanged;
+//     }
+// }
 
-namespace CyanStars.Gameplay.ChartEditor.BindableProperty
-{
-    /// <summary>
-    /// 只读的可观察属性接口
-    /// </summary>
-    public interface IReadonlyBindableProperty<out T>
-    {
-        /// <summary>
-        /// 获取当前值
-        /// </summary>
-        T Value { get; }
 
-        /// <summary>
-        /// 值变化时触发的事件
-        /// </summary>
-        event Action<T> OnValueChanged;
-    }
-}
