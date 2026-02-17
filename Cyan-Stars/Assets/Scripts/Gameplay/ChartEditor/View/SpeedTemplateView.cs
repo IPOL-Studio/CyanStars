4f488735074ac931698a256b4486bb5230a8d120
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-02-17 18:39:37 +0800

    Feat: 完善变速模板编辑区（除曲线编辑）

--- Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/View/SpeedTemplateView.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/View/SpeedTemplateView.cs
@@ -1,8 +1,10 @@
 ﻿#nullable enable
 
+using CyanStars.Chart;
 using CyanStars.Gameplay.ChartEditor.ViewModel;
 using ObservableCollections;
 using R3;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +12,12 @@ namespace CyanStars.Gameplay.ChartEditor.View
 {
     public class SpeedTemplateView : BaseView<SpeedTemplateViewModel>
     {
+        [SerializeField]
+        private Canvas canvas = null!;
+
+        [SerializeField]
+        private Button closeCanvasButton = null!;
+
         [SerializeField]
         private GameObject speedTemplateItemPrefab = null!;
 
@@ -19,10 +27,32 @@ namespace CyanStars.Gameplay.ChartEditor.View
         [SerializeField]
         private Button addSpeedTemplateItemButton = null!;
 
+        [SerializeField]
+        private GameObject detailFrameObject = null!;
+
+        [SerializeField]
+        private TMP_InputField remarkField = null!;
+
+        [SerializeField]
+        private Toggle relativeToggle = null!;
+
+        [SerializeField]
+        private Toggle absoluteToggle = null!;
+
+        [SerializeField]
+        private Button deleteButton = null!;
+
+        [SerializeField]
+        private Button cloneButton = null!;
+
 
         public override void Bind(SpeedTemplateViewModel targetViewModel)
         {
             base.Bind(targetViewModel);
+
+            closeCanvasButton.OnClickAsObservable()
+                .Subscribe(_ => canvas.enabled = false)
+                .AddTo(this);
 
             // 初始化时实例化已有的 item
             int index = 0;
@@ -34,6 +64,7 @@ namespace CyanStars.Gameplay.ChartEditor.View
                 index++;
             }
 
+            // VM->V 绑定列表元素
             ViewModel.SpeedTemplateData.ObserveAdd()
                 .Subscribe(e =>
                     {
@@ -52,9 +83,44 @@ namespace CyanStars.Gameplay.ChartEditor.View
                 )
                 .AddTo(this);
 
+            // VM->V 绑定编辑区
+            Observable.Merge(
+                    ViewModel.SelectedSpeedTemplateData,
+                    ViewModel.SelectedSpeedTemplateDataPropertyUpdatedSubject.Select(data => (SpeedTemplateData?)data)
+                )
+                .Subscribe(data =>
+                    {
+                        if (data == null)
+                        {
+                            detailFrameObject.SetActive(false);
+                            return;
+                        }
+
+                        detailFrameObject.SetActive(true);
+                        remarkField.text = data.Remark;
+                        if (data.Type == SpeedTemplateType.Relative) // Unity ToggleGroup 会自动关掉另一个
+                            relativeToggle.isOn = true;
+                        else
+                            absoluteToggle.isOn = true;
+                    }
+                )
+                .AddTo(this);
+
+            // V->VM 绑定
             addSpeedTemplateItemButton.OnClickAsObservable()
                 .Subscribe(_ => ViewModel.AddSpeedTemplateData())
                 .AddTo(this);
+            deleteButton.OnClickAsObservable()
+                .Subscribe(_ => ViewModel.DeleteSelectedSpeedTemplateData())
+                .AddTo(this);
+            cloneButton.OnClickAsObservable()
+                .Subscribe(_ => ViewModel.CloneSelectedSpeedTemplateData())
+                .AddTo(this);
+        }
+
+        public void OpenCanvas()
+        {
+            canvas.enabled = true;
         }
     }
 }
