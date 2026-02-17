226c7cc0c3a392b5e2e87b3e4dc2a087c5969b31
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-02-17 16:00:51 +0800

    Feat: 实现变速模板列表 item 的选择和添加

---
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/View/SpeedTemplateListItemView.cs
@@ -0,0 +1,45 @@
+﻿#nullable enable
+
+using CyanStars.Gameplay.ChartEditor.ViewModel;
+using R3;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CyanStars.Gameplay.ChartEditor.View
+{
+    public class SpeedTemplateListItemView : BaseView<SpeedTemplateListItemViewModel>
+    {
+        [SerializeField]
+        private Button itemButton = null!;
+
+        [SerializeField]
+        private Image ledImage = null!;
+
+        [SerializeField]
+        private TMP_Text countText = null!;
+
+        [SerializeField]
+        private TMP_Text remarkText = null!;
+
+
+        public override void Bind(SpeedTemplateListItemViewModel targetViewModel)
+        {
+            base.Bind(targetViewModel);
+
+            ViewModel.IsSelected
+                .Subscribe(isSelected => ledImage.enabled = isSelected)
+                .AddTo(this);
+            ViewModel.ItemIndex
+                .Subscribe(index => countText.text = $"#{index + 1}")
+                .AddTo(this);
+            ViewModel.PropertyUpdatedSubject
+                .Subscribe(data => remarkText.text = data.Name)
+                .AddTo(this);
+
+            itemButton.OnClickAsObservable()
+                .Subscribe(_ => ViewModel.OnClick())
+                .AddTo(this);
+        }
+    }
+}
