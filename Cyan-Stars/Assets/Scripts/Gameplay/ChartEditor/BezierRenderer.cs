a58b43c9e78f4f542e558837aae6c7c9689560dd
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-02-17 23:28:34 +0800

    Update

---
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/BezierRenderer.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using CyanStars.Utils.SpeedTemplate;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.ChartEditor
+{
+    /// <summary>
+    /// 用于变速模板的贝塞尔曲线渲染器
+    /// </summary>
+    public class BezierRenderer : MaskableGraphic
+    {
+        private const float Width = 2f; // 线条宽度
+        private static readonly Color SpeedCurveColor = Color.forestGreen;
+
+
+        public void DrawSpeedCurve(ICollection<BezierPoint> speedPoints)
+        {
+        }
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+        }
+    }
+}
