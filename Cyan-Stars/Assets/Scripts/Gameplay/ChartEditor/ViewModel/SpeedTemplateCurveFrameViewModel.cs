c0a8f680978a61019f8019940e1ec36f6a553831
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-02-18 22:46:47 +0800

    Update

--- Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/ViewModel/SpeedTemplateCurveFrameViewModel.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/ViewModel/SpeedTemplateCurveFrameViewModel.cs
@@ -1,12 +1,31 @@
 ﻿#nullable enable
 
+using CyanStars.Chart;
 using CyanStars.Gameplay.ChartEditor.Model;
+using CyanStars.Utils.SpeedTemplate;
+using R3;
 
 namespace CyanStars.Gameplay.ChartEditor.ViewModel
 {
     public class SpeedTemplateCurveFrameViewModel : BaseViewModel
     {
         private readonly SpeedTemplateViewModel SpeedTemplateViewModel;
+
+        public Subject<SpeedTemplateData> SelectedSpeedTemplateDataPropertyUpdatedSubject =>
+            SpeedTemplateViewModel.SelectedSpeedTemplateDataPropertyUpdatedSubject;
+
+        /// <summary>
+        /// 选中的变速模板中的贝塞尔曲线数据发生了变化
+        /// </summary>
+        public Subject<BezierCurve> SelectedBezierCurvePropertyUpdatedSubject = new();
+
+
+        /// <summary>
+        /// 当前选中的变速模板
+        /// </summary>
+        public ReadOnlyReactiveProperty<SpeedTemplateData?> SelectedSpeedTemplateData =>
+            SpeedTemplateViewModel.SelectedSpeedTemplateData;
+
 
         public SpeedTemplateCurveFrameViewModel(
             ChartEditorModel model,
