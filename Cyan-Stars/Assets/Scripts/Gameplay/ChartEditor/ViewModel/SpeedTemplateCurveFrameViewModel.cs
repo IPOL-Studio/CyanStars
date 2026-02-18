7a55135d917575f321624a47cfe91b258e833f79
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-02-18 17:25:35 +0800

    Update

---
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/ViewModel/SpeedTemplateCurveFrameViewModel.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+
+using CyanStars.Gameplay.ChartEditor.Model;
+
+namespace CyanStars.Gameplay.ChartEditor.ViewModel
+{
+    public class SpeedTemplateCurveFrameViewModel : BaseViewModel
+    {
+        private readonly SpeedTemplateViewModel SpeedTemplateViewModel;
+
+        public SpeedTemplateCurveFrameViewModel(
+            ChartEditorModel model,
+            SpeedTemplateViewModel speedTemplateViewModel
+        )
+            : base(model)
+        {
+            SpeedTemplateViewModel = speedTemplateViewModel;
+        }
+    }
+}
