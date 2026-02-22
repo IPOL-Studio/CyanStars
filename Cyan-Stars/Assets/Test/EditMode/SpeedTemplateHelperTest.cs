a04ffdb82e85d91fb657bf271b9b1b563c22fa81
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-02-22 16:50:12 +0800

    Squashed commit of the following:
    commit 9c16d741463951a429f7a9cb202a56d1df915fea
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Sun Feb 22 16:37:16 2026 +0800
    
        Feat: 优化查找性能，编写测试
    
    commit 5ae842ba92a316dbd33e41f71cfac0dd40bfe1ee
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Sun Feb 22 02:02:19 2026 +0800
    
        Test: BezierHelperTest 单元测试和修正
    
    commit db0453b3a4fccd2669c2b9fa400146735f69eb94
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Sat Feb 21 20:24:24 2026 +0800
    
        Update: 更新程序集
    
    commit 0dee6aef7c0c1ca6f4bedf71b9e1ee110884af4c
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Sat Feb 21 13:07:42 2026 +0800
    
        Rename
    
    commit 28acdf3ac84a5d98dd9859e311c754a64f245f4d
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Sat Feb 21 13:06:43 2026 +0800
    
        Update: 更新格式并添加拓展方法
    
    commit c45c87d59f03a02eb60a24eec61506ec364442e3
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Fri Feb 20 22:43:38 2026 +0800
    
        fix
    
    commit 5ab7bb82fe0403c2e0d4155798b7a039425e2f67
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Fri Feb 20 22:35:03 2026 +0800
    
        fix
    
    commit b3b5b136d69ed3f94aadf43b2085340043c9f909
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Fri Feb 20 22:17:12 2026 +0800
    
        Update: 重写采样逻辑
    
    commit 3e50f3c51807f6e1d8b42aab721482dc4fd71472
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Fri Feb 20 00:41:07 2026 +0800
    
        Move
    
    commit b82b77e599c69c0bf416bd46eeb6929690e638b2
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Tue Feb 17 12:32:59 2026 +0800
    
        Update
    
    commit 4292ad31717700df73f255b241e2b4ad1e01d0d8
    Author: CL <62653664+Chen-Luan@users.noreply.github.com>
    Date:   Mon Feb 16 18:17:51 2026 +0800
    
        Enhance: 重写贝塞尔曲线相关逻辑

---
+++ Cyan-Stars/Assets/Test/EditMode/SpeedTemplateHelperTest.cs
@@ -0,0 +1,212 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using CyanStars.Chart;
+using CyanStars.Chart.BezierCurve;
+using NUnit.Framework;
+
+namespace Test.EditMode
+{
+    /// <summary>
+    /// 测试 SpeedTemplateHelper 是否正常烘焙贝塞尔曲线组在采样点时的速度和位移
+    /// </summary>
+    public class SpeedTemplateHelperTest
+    {
+        /// <summary>
+        /// 测试位移时允许的误差
+        /// </summary>
+        private const float DisplacementEpsilon = 0.001f;
+
+        /// <summary>
+        /// 测试速度时允许的误差
+        /// </summary>
+        private const float SpeedEpsilon = 0.001f;
+
+
+        // 准备用于测试的贝塞尔曲线组
+        private static BezierCurves bezierCurves1 =
+            new BezierCurves(
+                new BezierPoint(
+                    new BezierPointPos(0, 0f),
+                    new BezierPointPos(0, 0f),
+                    new BezierPointPos(0, 0f)
+                )
+            );
+
+        private static BezierCurves bezierCurves2 =
+            new BezierCurves(
+                new BezierPoint(
+                    new BezierPointPos(0, 0f),
+                    new BezierPointPos(0, 0f),
+                    new BezierPointPos(0, 0f)
+                )
+            )
+            {
+                new BezierPoint(
+                    new BezierPointPos(5, 10000f),
+                    new BezierPointPos(5, 10000f),
+                    new BezierPointPos(5, 10000f)
+                ),
+                new BezierPoint(
+                    new BezierPointPos(10, 0f),
+                    new BezierPointPos(10, 0f),
+                    new BezierPointPos(10, 0f)
+                ),
+                new BezierPoint(
+                    new BezierPointPos(15, -10000f),
+                    new BezierPointPos(15, -10000f),
+                    new BezierPointPos(15, -10000f)
+                ),
+                new BezierPoint(
+                    new BezierPointPos(20, 0f),
+                    new BezierPointPos(20, 0f),
+                    new BezierPointPos(20, 0f)
+                ),
+                new BezierPoint(
+                    new BezierPointPos(40, 10000f),
+                    new BezierPointPos(40, 10000f),
+                    new BezierPointPos(40, 10000f)
+                )
+            };
+
+
+        private static IEnumerable<TestCaseData> SampleCountTestCase
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Absolute, bezierCurves1),
+                        2f,
+                        SpeedTemplateHelper.SampleIntervalMsTime,
+                        0
+                    )
+                    .SetName("单点曲线采样数量测试 1");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Relative, bezierCurves1),
+                        2f,
+                        SpeedTemplateHelper.SampleIntervalMsTime,
+                        0
+                    )
+                    .SetName("单点曲线采样数量测试 2");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Absolute, bezierCurves2),
+                        2f,
+                        SpeedTemplateHelper.SampleIntervalMsTime,
+                        4
+                    )
+                    .SetName("多点曲线采样数量测试 1");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Relative, bezierCurves2),
+                        2f,
+                        SpeedTemplateHelper.SampleIntervalMsTime,
+                        4
+                    )
+                    .SetName("多点曲线采样数量测试 2");
+            }
+        }
+
+        private static IEnumerable<TestCaseData> GetFinalDisplacementTestCase
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Absolute, bezierCurves1),
+                        2f,
+                        0
+                    )
+                    .SetName("单点曲线位移测试 1");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Relative, bezierCurves1),
+                        2f,
+                        0
+                    )
+                    .SetName("单点曲线位移测试 2");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Absolute, bezierCurves2),
+                        2f,
+                        100
+                    )
+                    .SetName("多点曲线位移测试 1");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Relative, bezierCurves2),
+                        2f,
+                        200
+                    )
+                    .SetName("多点曲线位移测试 2");
+            }
+        }
+
+        private static IEnumerable<TestCaseData> SampleValuesTestCase // TODO: 之后整点随机精确数据试试
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Absolute, bezierCurves1),
+                        2f,
+                        new float[] { },
+                        new float[] { }
+                    )
+                    .SetName("单点曲线采样测试 1");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Relative, bezierCurves1),
+                        2f,
+                        new float[] { },
+                        new float[] { }
+                    )
+                    .SetName("单点曲线采样测试 2");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Absolute, bezierCurves2),
+                        2f,
+                        new[] { 0f, 0f, 0f, 5000f },
+                        new[] { 0f, 50f, 0f, 25f }
+                    )
+                    .SetName("多点曲线采样测试 1");
+                yield return new TestCaseData(
+                        new SpeedTemplateData(SpeedGroupType.Relative, bezierCurves2),
+                        2f,
+                        new[] { 0f, 0f, 0f, 10000f },
+                        new[] { 0f, 100f, 0f, 50f }
+                    )
+                    .SetName("多点曲线采样测试 2");
+            }
+        }
+
+
+        /// <summary>
+        /// 测试采样点数量是否正确
+        /// </summary>
+        [Test, TestCaseSource(nameof(SampleCountTestCase))]
+        public void SampleCountTest(SpeedTemplateData speedTemplateData, float playerSpeed, int sampleIntervalMsTime, int expectedResult)
+        {
+            SpeedTemplateHelper.Bake(speedTemplateData, playerSpeed, out List<float> speedList, out List<float> displacementList);
+            Assert.AreEqual(expectedResult, speedList.Count);
+            Assert.AreEqual(expectedResult, displacementList.Count);
+        }
+
+        /// <summary>
+        /// 测试计算整组曲线最终位移是否正确
+        /// </summary>
+        [Test, TestCaseSource(nameof(GetFinalDisplacementTestCase))]
+        public void GetFinalDisplacementTest(SpeedTemplateData speedTemplateData, float playerSpeed, double expectedResult)
+        {
+            double displacement = SpeedTemplateHelper.GetFinalDisplacement(speedTemplateData, playerSpeed);
+            Assert.AreEqual(expectedResult, displacement, DisplacementEpsilon);
+        }
+
+        /// <summary>
+        /// 测试采样点结果是否正确
+        /// </summary>
+        [Test, TestCaseSource(nameof(SampleValuesTestCase))]
+        public void SampleValuesTest(SpeedTemplateData speedTemplateData, float playerSpeed, float[] speeds, float[] displacements)
+        {
+            SpeedTemplateHelper.Bake(speedTemplateData, playerSpeed, out List<float> speedList, out List<float> displacementList);
+
+            for (int i = 0; i < Math.Max(speedList.Count, speeds.Length); i++)
+            {
+                Assert.AreEqual(speeds[i], speedList[i], SpeedEpsilon);
+                Assert.AreEqual(displacements[i], displacementList[i], DisplacementEpsilon);
+            }
+        }
+    }
+}
