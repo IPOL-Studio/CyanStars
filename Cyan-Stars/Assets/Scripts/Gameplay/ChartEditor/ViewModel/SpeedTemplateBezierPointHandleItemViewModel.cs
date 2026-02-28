a57c5649aa724b1f53136270337759159fdd13da
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-02-28 15:11:11 +0800

    [WIP]Feat: 添加贝塞尔点逻辑

---
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/ViewModel/SpeedTemplateBezierPointHandleItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+using CyanStars.Chart.BezierCurve;
+using CyanStars.Gameplay.ChartEditor.Model;
+using R3;
+
+namespace CyanStars.Gameplay.ChartEditor.ViewModel
+{
+    public class SpeedTemplateBezierPointHandleItemViewModel : BaseViewModel
+    {
+        private readonly SpeedTemplateCurveFrameViewModel SpeedTemplateCurveFrameViewModel;
+        private readonly BezierPoint BezierPoint;
+
+        public readonly ReactiveProperty<bool> SelfSelected = new ReactiveProperty<bool>();
+
+
+        public SpeedTemplateBezierPointHandleItemViewModel(
+            ChartEditorModel model,
+            SpeedTemplateCurveFrameViewModel speedTemplateCurveFrameViewModel,
+            BezierPoint bezierPoint
+        )
+            : base(model)
+        {
+            SpeedTemplateCurveFrameViewModel = speedTemplateCurveFrameViewModel;
+            BezierPoint = bezierPoint;
+
+            SpeedTemplateCurveFrameViewModel.SelectedPoint
+                .Subscribe(selectedPoint => SelfSelected.Value = selectedPoint == BezierPoint)
+                .AddTo(base.Disposables);
+        }
+    }
+}
