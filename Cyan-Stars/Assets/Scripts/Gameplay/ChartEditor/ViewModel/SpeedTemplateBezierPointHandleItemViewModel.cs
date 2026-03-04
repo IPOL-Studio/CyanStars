6268cac50ccf2397dea4d9b949ea1a3b37127baf
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-03-04 17:12:14 +0800

    [WIP] Update

--- Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/ViewModel/SpeedTemplateBezierPointHandleItemViewModel.cs
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/ViewModel/SpeedTemplateBezierPointHandleItemViewModel.cs
@@ -24,7 +24,7 @@ namespace CyanStars.Gameplay.ChartEditor.ViewModel
         public ReadOnlyReactiveProperty<float> OffsetX => SpeedTemplateCurveFrameViewModel.OffsetX;
         public ReadOnlyReactiveProperty<float> OffsetY => SpeedTemplateCurveFrameViewModel.OffsetY;
 
-        private Vector2? recordedLocalPoint = null;
+        private BezierPoint? recordedLocalPoint = null;
         private BezierPointSubItemType? recordedType = null!;
 
 
@@ -71,10 +71,10 @@ namespace CyanStars.Gameplay.ChartEditor.ViewModel
             );
         }
 
-        public void RecordSubPointPos(Vector2 localPoint, BezierPointSubItemType type)
+        public void RecordSubPointPos(BezierPointSubItemType draggingHandleType)
         {
-            recordedLocalPoint = localPoint;
-            recordedType = type;
+            recordedLocalPoint = BezierPointWrapper.CurrentValue;
+            recordedType = draggingHandleType;
         }
 
         public void CommitSubPointPos(Vector2 newLocalPoint, BezierPointSubItemType type)
@@ -85,7 +85,7 @@ namespace CyanStars.Gameplay.ChartEditor.ViewModel
                 throw new Exception("结束拖拽时的 type 与开始拖拽时记录的不一致，请检查！");
 
             GetPointDataByLocalPoint(newLocalPoint, type, out BezierPoint newBezierPoint);
-            GetPointDataByLocalPoint((Vector2)recordedLocalPoint, type, out BezierPoint oldBezierPoint);
+            BezierPoint oldBezierPoint = (BezierPoint)recordedLocalPoint;
 
             CommandStack.ExecuteCommand(
                 () =>
@@ -108,27 +108,44 @@ namespace CyanStars.Gameplay.ChartEditor.ViewModel
             recordedType = null;
         }
 
+        /// <summary>
+        /// 根据当前的局部位置和拖动点种类，计算贝塞尔点三个子控制点的坐标
+        /// </summary>
+        /// <remarks>最终输出的坐标会确保整条曲线合法不折返，且拖动位置点时会同步更新左右控制点位置</remarks>
         private void GetPointDataByLocalPoint(Vector2 localPoint, BezierPointSubItemType type, out BezierPoint bezierPoint)
         {
             // 将本地坐标转换到贝塞尔点数据位置
-            int msTime = (int)(localPoint.x / SpeedTemplateCurveFrameViewModel.ScaleX.CurrentValue - SpeedTemplateCurveFrameViewModel.OffsetX.CurrentValue);
-            float value = localPoint.y / SpeedTemplateCurveFrameViewModel.ScaleY.CurrentValue - SpeedTemplateCurveFrameViewModel.OffsetY.CurrentValue;
+            float msTime =
+                localPoint.x / SpeedTemplateCurveFrameViewModel.ScaleX.CurrentValue
+                - SpeedTemplateCurveFrameViewModel.OffsetX.CurrentValue;
+            float value =
+                localPoint.y / SpeedTemplateCurveFrameViewModel.ScaleY.CurrentValue
+                - SpeedTemplateCurveFrameViewModel.OffsetY.CurrentValue;
 
-            // TODO: 做个验证避免错误数据抛异常
+            // TODO: 结合整条贝塞尔曲线组，将初步转换的数据进一步转换到合法的数据，避免异常
 
             switch (type)
             {
                 case BezierPointSubItemType.PosPoint:
+                    float offsetMsTime = msTime - ((BezierPoint)recordedLocalPoint).PositionPoint.MsTime;
+                    float offsetValue = value - ((BezierPoint)recordedLocalPoint).PositionPoint.Value;
+
                     bezierPoint = new BezierPoint(
-                        new BezierPointPos(msTime, value),
-                        new BezierPointPos(BezierPointWrapper.CurrentValue.LeftControlPoint.MsTime, BezierPointWrapper.CurrentValue.LeftControlPoint.Value),
-                        new BezierPointPos(BezierPointWrapper.CurrentValue.RightControlPoint.MsTime, BezierPointWrapper.CurrentValue.RightControlPoint.Value)
+                        new BezierPointPos((int)msTime, value),
+                        new BezierPointPos(
+                            ((BezierPoint)recordedLocalPoint).LeftControlPoint.MsTime + (int)offsetMsTime,
+                            ((BezierPoint)recordedLocalPoint).LeftControlPoint.Value + offsetValue
+                        ),
+                        new BezierPointPos(
+                            ((BezierPoint)recordedLocalPoint).RightControlPoint.MsTime + (int)offsetMsTime,
+                            ((BezierPoint)recordedLocalPoint).RightControlPoint.Value + offsetValue
+                        )
                     );
                     break;
                 case BezierPointSubItemType.LeftControlPoint:
                     bezierPoint = new BezierPoint(
                         new BezierPointPos(BezierPointWrapper.CurrentValue.PositionPoint.MsTime, BezierPointWrapper.CurrentValue.PositionPoint.Value),
-                        new BezierPointPos(msTime, value),
+                        new BezierPointPos((int)msTime, value),
                         new BezierPointPos(BezierPointWrapper.CurrentValue.RightControlPoint.MsTime, BezierPointWrapper.CurrentValue.RightControlPoint.Value)
                     );
                     break;
@@ -136,7 +153,7 @@ namespace CyanStars.Gameplay.ChartEditor.ViewModel
                     bezierPoint = new BezierPoint(
                         new BezierPointPos(BezierPointWrapper.CurrentValue.PositionPoint.MsTime, BezierPointWrapper.CurrentValue.PositionPoint.Value),
                         new BezierPointPos(BezierPointWrapper.CurrentValue.LeftControlPoint.MsTime, BezierPointWrapper.CurrentValue.LeftControlPoint.Value),
-                        new BezierPointPos(msTime, value)
+                        new BezierPointPos((int)msTime, value)
                     );
                     break;
                 default:
