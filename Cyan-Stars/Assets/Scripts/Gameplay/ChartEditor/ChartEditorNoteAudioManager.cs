5b9d0418d84278f1cd69bd6ea7db4c6a77e646bd
memorydream <memorydream.chen@outlook.com>
2026-03-05 16:10:26 +0800

    feat: 为制谱器添加一个简单且 hack 的 note 打击音实现

---
+++ Cyan-Stars/Assets/Scripts/Gameplay/ChartEditor/ChartEditorNoteAudioManager.cs
@@ -0,0 +1,112 @@
+using CyanStars.Gameplay.ChartEditor.Model;
+using UnityEngine;
+using R3;
+using CyanStars.Chart;
+using System.Collections.Generic;
+using CyanStars.Framework;
+using CatAsset.Runtime;
+using System;
+
+namespace CyanStars.Gameplay.ChartEditor
+{
+    // TODO: 这个类现在的实现非常 hack，只是为了让制谱器有打击音可用
+    // 没有优化，也无扩展
+    public class ChartEditorNoteAudioManager : MonoBehaviour
+    {
+        private ChartEditorModel model = null!;
+        private int prevTimeMs = 0;
+        private int skipNoteCount = 0;
+
+        private const string FallbackAudioAssetName = "Assets/BundleRes/Audio/PromptTone/ns_ka.ogg";
+        // 预加载内置提示音，但在实际播放时不会使用这个 handler
+        // 只是为了避免被资源框架卸载
+        private AssetHandler<AudioClip> fallbackAudioAssetHandler;
+
+        private float audioVolume = 1f;
+        public float AudioVolume
+        {
+            get => audioVolume;
+            set
+            {
+                audioVolume = Math.Clamp(value, 0f, 1f);
+            }
+        }
+
+        public void Init(ChartEditorModel chartEditorModel)
+        {
+            fallbackAudioAssetHandler = GameRoot.Asset.LoadAssetAsync<AudioClip>(FallbackAudioAssetName).AddTo(this);
+
+            chartEditorModel.IsTimelinePlaying
+                .Subscribe(toPlay =>
+                {
+                    if (!toPlay)
+                        return;
+
+                    prevTimeMs = model.CurrentTimelineTimeMs;
+                    skipNoteCount = GetSkippedNoteCount(prevTimeMs);
+                })
+                .AddTo(this);
+
+            model = chartEditorModel;
+        }
+
+        private int GetSkippedNoteCount(int toTimeMs)
+        {
+            var notes = model.ChartData.CurrentValue.Notes;
+            var bpmGroups = model.ChartPackData.CurrentValue.BpmGroup;
+            int count = 0;
+            foreach (var note in notes)
+            {
+                var judgeTime = BpmGroupHelper.CalculateTime(bpmGroups, note.JudgeBeat);
+                if (judgeTime > toTimeMs)
+                    break;
+
+                count++;
+            }
+            return count;
+        }
+
+        public void LateUpdate()
+        {
+            if (model is null || !model.IsTimelinePlaying.CurrentValue)
+                return;
+
+            if (model.CurrentTimelineTimeMs == prevTimeMs)
+                return;
+
+            if (skipNoteCount >= model.ChartData.CurrentValue.Notes.Count)
+                return;
+
+            // 作为临时的 hack 实现，这里可以选择只收集需要播放的数量
+            // 但还是直接把所有 note 收集起来
+            // 之后可以看看能不能把 notes 的运行时存储改为时间轮
+            foreach (var note in CollectHitedNotes(model.CurrentTimelineTimeMs))
+            {
+                GameRoot.Audio.Play2DSound(FallbackAudioAssetName, AudioVolume);
+            }
+        }
+
+        public IEnumerable<BaseChartNoteData> CollectHitedNotes(int toTimeMs)
+        {
+            var notes = model.ChartData.CurrentValue.Notes;
+            var bpmGroups = model.ChartPackData.CurrentValue.BpmGroup;
+            int index = skipNoteCount;
+            while (index < notes.Count)
+            {   
+                var note = notes[index];
+                var judgeTime = BpmGroupHelper.CalculateTime(bpmGroups, note.JudgeBeat);
+
+                if (note.JudgeAble)
+                {
+                    if (judgeTime > toTimeMs)
+                        break;
+
+                    yield return note;
+                }
+
+                index++;
+            }
+            skipNoteCount = index;
+        }
+    }
+}
