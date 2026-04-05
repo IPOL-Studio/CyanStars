1b07fed64def27c618ec2b75cca1e1d759335c3d
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-04-05 16:31:56 +0800

    Update: 暂时停用变速模板，修正gameplay位置计算 (#337)

--- Cyan-Stars/Assets/Scripts/Chart/SpeedTemplate.cs
+++ Cyan-Stars/Assets/Scripts/Chart/SpeedTemplate.cs
@@ -11,82 +11,87 @@ namespace CyanStars.Chart
     public sealed class SpeedTemplate
     {
         private readonly SpeedTemplateData SpeedTemplateData;
+        private readonly float PlayerSpeed;
 
-        /// <summary>
-        /// 缓存的速度列表
-        /// </summary>
-        /// <remarks>列表从 0 时间差开始，每隔 SampleInterval 个时间向前采样，计算提前一段时间的速度</remarks>
-        private readonly List<float> SpeedList;
-
-        /// <summary>
-        /// 缓存的距离（视图层时间）列表
-        /// </summary>
-        /// <remarks>列表从 0 时间差开始，每隔 SampleInterval 个时间向前采样，计算提前一段时间的距离</remarks>
-        private readonly List<float> DisplacementList;
-
-        /// <summary>
-        /// 缓存的最远端贝塞尔点的最终位移
-        /// </summary>
-        private readonly double FinalDisplacement;
-
-
-        private SpeedTemplate(SpeedTemplateData speedTemplateData, List<float> speedList, List<float> displacementList, double finalDisplacement)
+        public SpeedTemplate(float playerSpeed)
         {
-            SpeedTemplateData = speedTemplateData;
-            SpeedList = speedList;
-            DisplacementList = displacementList;
-            FinalDisplacement = finalDisplacement;
+            PlayerSpeed = playerSpeed;
         }
 
-        // TODO: 外部传入玩家速度
-        /// <summary>
-        /// 构造实例并烘焙速度和距离采样点
-        /// </summary>
-        public static SpeedTemplate Create(SpeedTemplateData data, ISpeedTemplateBaker baker, float playerSpeed = 1f)
-        {
-            _ = data ?? throw new ArgumentNullException(nameof(data));
-            _ = baker ?? throw new ArgumentNullException(nameof(baker));
+        // TODO: 暂时未使用速度模板，等待后续将变速整合进曲线模板后统一采用曲线位置，而非根据速度计算位置
+        // /// <summary>
+        // /// 缓存的速度列表
+        // /// </summary>
+        // /// <remarks>列表从 0 时间差开始，每隔 SampleInterval 个时间向前采样，计算提前一段时间的速度</remarks>
+        // private readonly List<float> SpeedList;
+        //
+        // /// <summary>
+        // /// 缓存的距离（视图层时间）列表
+        // /// </summary>
+        // /// <remarks>列表从 0 时间差开始，每隔 SampleInterval 个时间向前采样，计算提前一段时间的距离</remarks>
+        // private readonly List<float> DisplacementList;
+        //
+        // /// <summary>
+        // /// 缓存的最远端贝塞尔点的最终位移
+        // /// </summary>
+        // private readonly double FinalDisplacement;
 
-            baker.Bake(
-                data,
-                playerSpeed,
-                out List<float> speedList,
-                out List<float> displacementList
-            );
-            double finalDisplacement = baker.GetFinalDisplacement(data, playerSpeed);
-            return new SpeedTemplate(data, speedList, displacementList, finalDisplacement);
-        }
+        // private SpeedTemplate(SpeedTemplateData speedTemplateData, List<float> speedList, List<float> displacementList, double finalDisplacement)
+        // {
+        //     SpeedTemplateData = speedTemplateData;
+        //     SpeedList = speedList;
+        //     DisplacementList = displacementList;
+        //     FinalDisplacement = finalDisplacement;
+        // }
+
+        // /// <summary>
+        // /// 构造实例并烘焙速度和距离采样点
+        // /// </summary>
+        // public static SpeedTemplate Create(SpeedTemplateData data, ISpeedTemplateBaker baker, float playerSpeed)
+        // {
+        //     _ = data ?? throw new ArgumentNullException(nameof(data));
+        //     _ = baker ?? throw new ArgumentNullException(nameof(baker));
+        //
+        //     baker.Bake(
+        //         data,
+        //         playerSpeed,
+        //         out List<float> speedList,
+        //         out List<float> displacementList
+        //     );
+        //     double finalDisplacement = baker.GetFinalDisplacement(data, playerSpeed);
+        //     return new SpeedTemplate(data, speedList, displacementList, finalDisplacement);
+        // }
 
         /// <summary>
         /// 根据相对时间计算音符当前的位移
         /// </summary>
         public float GetDistance(float msTime)
         {
-            msTime = msTime * -1; // TODO: 历史遗留问题，逻辑层时间提前时为负数
+            return PlayerSpeed * msTime / 1000f;
 
-            if (msTime <= 0)
-            {
-                // 音符已经超过判定时间，根据 [0] 贝塞尔点线性计算位移
-                var speed = SpeedTemplateData.BezierCurves[0].PositionPoint.Value;
-                return speed * msTime / 1000f;
-            }
-            else if (SpeedTemplateData.BezierCurves[^1].PositionPoint.Value <= msTime)
-            {
-                // 音符还未达到判定时间，且在最远端贝塞尔点之外
-                // 线性计算超出贝塞尔点时间部分的位移，然后加上整段位移
-                var segmentMsTime = msTime - SpeedTemplateData.BezierCurves[^1].PositionPoint.MsTime;
-                var speed = SpeedTemplateData.BezierCurves[^1].PositionPoint.Value;
-                return (float)FinalDisplacement + speed * segmentMsTime / 1000f;
-            }
-            else
-            {
-                // 音符落在曲线上，直接根据采样结果返回
-                // 将音符对齐到最近的采样点上
-                const float halfSampleIntervalMsTime = CacheSpeedTemplateBaker.SampleIntervalMsTime / 2f;
-                int sampleIndex = (int)((msTime + halfSampleIntervalMsTime) / CacheSpeedTemplateBaker.SampleIntervalMsTime);
-                sampleIndex = Math.Min(sampleIndex, DisplacementList.Count - 1);
-                return DisplacementList[sampleIndex];
-            }
+            // if (msTime <= 0)
+            // {
+            //     // 音符已经超过判定时间，根据 [0] 贝塞尔点线性计算位移
+            //     var speed = SpeedTemplateData.BezierCurves[0].PositionPoint.Value;
+            //     return speed * msTime / 1000f;
+            // }
+            // else if (SpeedTemplateData.BezierCurves[^1].PositionPoint.Value <= msTime)
+            // {
+            //     // 音符还未达到判定时间，且在最远端贝塞尔点之外
+            //     // 线性计算超出贝塞尔点时间部分的位移，然后加上整段位移
+            //     var segmentMsTime = msTime - SpeedTemplateData.BezierCurves[^1].PositionPoint.MsTime;
+            //     var speed = SpeedTemplateData.BezierCurves[^1].PositionPoint.Value;
+            //     return (float)FinalDisplacement + speed * segmentMsTime / 1000f;
+            // }
+            // else
+            // {
+            //     // 音符落在曲线上，直接根据采样结果返回
+            //     // 将音符对齐到最近的采样点上
+            //     const float halfSampleIntervalMsTime = CacheSpeedTemplateBaker.SampleIntervalMsTime / 2f;
+            //     int sampleIndex = (int)((msTime + halfSampleIntervalMsTime) / CacheSpeedTemplateBaker.SampleIntervalMsTime);
+            //     sampleIndex = Math.Min(sampleIndex, DisplacementList.Count - 1);
+            //     return DisplacementList[sampleIndex];
+            // }
         }
     }
 }
