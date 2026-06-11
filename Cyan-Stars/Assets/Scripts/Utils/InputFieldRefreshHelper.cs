e25b39840c1d647eb52d27c99f992fda2b01e82c
CL <62653664+Chen-Luan@users.noreply.github.com>
2026-06-11 15:49:09 +0800

    Fix: 谱包简介输入框初始化时未正确设置高度 (#402)
    * Fix: 谱包简介输入框初始化时未正确设置高度
    
    * Update InputFieldRefreshHelper.cs

--- Cyan-Stars/Assets/Scripts/Utils/InputFieldRefreshHelper.cs
+++ Cyan-Stars/Assets/Scripts/Utils/InputFieldRefreshHelper.cs
@@ -2,6 +2,7 @@
 
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace CyanStars
@@ -11,7 +12,7 @@ namespace CyanStars
     /// </summary>
     [RequireComponent(typeof(TMP_InputField))]
     [RequireComponent(typeof(LayoutElement))]
-    public class InputFieldRefreshHelper : MonoBehaviour
+    public class InputFieldRefreshHelper : UIBehaviour
     {
         [SerializeField]
         private float verticalPadding = 30f;
@@ -19,28 +20,61 @@ namespace CyanStars
         private TMP_InputField tmpInputField = null!;
         private LayoutElement layoutElement = null!;
 
-        private void Awake()
+        protected override void Awake()
         {
+            base.Awake();
             tmpInputField = GetComponent<TMP_InputField>();
             layoutElement = GetComponent<LayoutElement>();
         }
 
-        private void Start() => CalculateHight(tmpInputField.text); // 初始化调用一次
-
-        private void OnEnable() => tmpInputField.onValueChanged.AddListener(CalculateHight);
-        private void OnDisable() => tmpInputField.onValueChanged.RemoveListener(CalculateHight);
-
-        private void CalculateHight(string text)
+        protected override void Start()
         {
+            base.Start();
+            CalculateHeight(tmpInputField.text); // 初始化调用一次
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            tmpInputField.onValueChanged.AddListener(CalculateHeight);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            tmpInputField.onValueChanged.RemoveListener(CalculateHeight);
+        }
+
+
+        protected override void OnRectTransformDimensionsChange()
+        {
+            base.OnRectTransformDimensionsChange();
+
+            if (tmpInputField != null && isActiveAndEnabled)
+            {
+                CalculateHeight(tmpInputField.text);
+            }
+        }
+
+
+        private void CalculateHeight(string text)
+        {
+            float textWidth = tmpInputField.textComponent.rectTransform.rect.width;
+            if (textWidth <= 0.01f)
+                return; // TMP 输入框尚未完成初始化
+
             // 末尾是换行符和空格等不可见字符的情况时，临时追加一个字符用于高度计算。
             string calcText = text;
             if (calcText.EndsWith("\n") || calcText.EndsWith(" "))
                 calcText += ".";
 
-            float textWidth = tmpInputField.textComponent.rectTransform.rect.width;
             float textHeight = tmpInputField.textComponent.GetPreferredValues(calcText, textWidth, 0).y;
             textHeight += verticalPadding;
-            layoutElement.preferredHeight = Mathf.Max(layoutElement.minHeight, textHeight);
+
+            float newHeight = Mathf.Max(layoutElement.minHeight, textHeight);
+
+            if (Mathf.Abs(layoutElement.preferredHeight - newHeight) > 0.1f)
+                layoutElement.preferredHeight = newHeight;
         }
     }
 }
