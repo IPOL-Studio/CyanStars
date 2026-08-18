0208373f4c9ba0b183f08401dea70b904607306d
memorydream <memorydream.chen@outlook.com>
2026-08-18 23:30:35 +0800

    fix: 重做 AtParagraph 解析，解决边缘情况 (#424)

--- Cyan-Stars/Assets/Scripts/Libraries/MarkdownRenderer/Runtime/Extensions/AtParagraph/AtParagraphInline.cs
+++ Cyan-Stars/Assets/Scripts/Libraries/MarkdownRenderer/Runtime/Extensions/AtParagraph/AtParagraphInline.cs
@@ -4,13 +4,6 @@ namespace CyanStars.MarkdownRenderer.Extensions.AtParagraph
 {
     public sealed class AtParagraphInline : LinkInline
     {
-        public AtParagraphInline(string paragraph)
-        {
-            Paragraph = paragraph;
-            Label = "@" + paragraph;
-            Url = string.Empty;
-        }
-
-        public string Paragraph { get; }
+        public string Paragraph {get; set;}
     }
 }
